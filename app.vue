cf1e46ebdfc8db2636a7bf0f6312c2613019b790
pinkie <hongvo@ycomm.vn>
2023-12-22 10:38:51 +0700

    setup scss directory

--- app.vue
+++ app.vue
@@ -3,3 +3,6 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style lang="scss">
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,15 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/i18n'],
   i18n: {
     vueI18n: './plugin/i18n.config.ts'
+  },
+  css: ['~/assets/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/variables.scss" as *;'
+        }
+      }
+    }
   }
 })

822c5655f2b4d9ff2c20071856db3841473841c4
pinkie <hongvo@ycomm.vn>
2023-12-22 13:53:54 +0700

    move i18n.config outside

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
   modules: ['@nuxtjs/i18n', '@nuxtjs/svg-sprite', '@vueuse/nuxt', '@pinia/nuxt'],
-  i18n: {
-    vueI18n: './plugin/i18n.config.ts'
-  },
   css: ['~/assets/scss/main.scss'],
   vite: {
     css: {
